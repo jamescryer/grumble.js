2d38753a29cc1431019d9796ea48a9fc6b439a59
D. Dotsenko <dotsa@hotmail.com>
2011-12-19 19:47:42 -0800

    Folding the jQuery and GrumbleBubble inside closures so that the Grumble can be loaded in AMD-compatible loaders like RequireJS, where 'window' is not 'this' context for script eval.

--- js/Bubble.js
+++ js/Bubble.js
@@ -1,9 +1,10 @@
 /**   (c) 2011 James Cryer, Huddle (www.huddle.com) 	*/
 /**   http://jamescryer.github.com/grumble.js/ 		*/
 
-(function($, window){
+(function(window){
 
-    var defaults = {
+    var $ = window.jQuery, 
+	defaults = {
         type: '',
         text: '',
         top: 0,
@@ -151,6 +152,5 @@
 			}
 			return template;
 		}
-		
     };
-}($, window));
+}(window));
